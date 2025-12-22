431a68b490ea020083e0649890b4eb2fd654dba0
Smruti-1163 <nayaksmrutiranjan274@gmail.com>
2025-12-23 04:22:28 +0530

    Fix output message for AXIS bank interest rate

--- java/Assignment - 5/06_BankDemo.java
+++ java/Assignment - 5/06_BankDemo.java
@@ -27,6 +27,7 @@ public class BankDemo {
         b = new ICICI();
         System.out.println("ICICI Rate of Interest : " + b.getRateOfInterest());
         b = new AXIS();
-        System.out.println("SBI Rate of Interest : " + b.getRateOfInterest());
+        System.out.println("AXIS Rate of Interest : " + b.getRateOfInterest());
     }
 }
+
